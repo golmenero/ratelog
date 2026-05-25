35d524cc547b46eb817cf08c270300901467ce94
Carlos Gomez <carlosgomezcolmenero@gmail.com>
2026-05-25 17:19:27 +0200

    [SEASONS] Minimal refactor

--- src/main/kotlin/org/raterr/user/UserRepository.kt
+++ src/main/kotlin/org/raterr/user/UserRepository.kt
@@ -3,6 +3,16 @@ package org.raterr.user
 import org.raterr.Email
 import org.raterr.Username
 
+data class User(
+    val id: Id?,
+    val username: Username,
+    val email: Email,
+    val passwordHash: String,
+    val createdAtEpochMs: Long = System.currentTimeMillis()
+) {
+    data class Id(val value: Long)
+}
+
 interface UserRepository {
     fun findById(id: User.Id): User?
     fun findByUsername(username: Username): User?
