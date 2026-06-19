5131a88cb5fd02268282604d1d0f8cbba7234b48
Carlos Gomez <carlosgomezcolmenero@gmail.com>
2026-06-19 20:24:33 +0200

    [FIX] Fix tests

--- src/test/kotlin/org/ratelog/user/feed/FeedHandlerTest.kt
+++ src/test/kotlin/org/ratelog/user/feed/FeedHandlerTest.kt
@@ -39,6 +39,7 @@ class FeedHandlerTest {
     fun `should return feed items from followed users movie ratings`() {
         val followedUser = UserFactory.aUser(id = 2, username = "followeduser", email = "followed@example.com")
         userRepository.save(followedUser)
+        userRepository.toggleFollow(User.Id(1), User.Id(2))
 
         val rating = RatingFactory.aRating(movieId = Movie.Id(1), userId = User.Id(2), directing = 5.0, cinematography = 5.0, acting = 5.0, soundtrack = 5.0, screenplay = 5.0, createdAt = Instant.now(), review = null)
         ratingRepository.save(rating)
@@ -55,6 +56,7 @@ class FeedHandlerTest {
     fun `should return feed items from followed users tv ratings`() {
         val followedUser = UserFactory.aUser(id = 2, username = "followeduser", email = "followed@example.com")
         userRepository.save(followedUser)
+        userRepository.toggleFollow(User.Id(1), User.Id(2))
 
         val seasonRating = TvRatingFactory.aSeasonRating(
             tvShowId = TvShow.Id(1),
