a9740fa56f8bcd567d65441dbf82671e41882da1
Carlos Gomez <carlosgomezcolmenero@gmail.com>
2026-07-12 14:35:14 +0200

    [I18N] Unify migrations

--- src/main/resources/db/migration/V15__remove_title_and_overview_from_media.sql
+++ src/main/resources/db/migration/V15__remove_title_and_overview_from_media.sql
@@ -1,3 +1,8 @@
+UPDATE movies SET original_title = title WHERE original_title IS NULL;
+UPDATE tv SET original_name = name WHERE original_name IS NULL;
+
+ALTER TABLE movies ALTER COLUMN original_title SET NOT NULL;
+ALTER TABLE tv ALTER COLUMN original_name SET NOT NULL;
 ALTER TABLE movies DROP COLUMN IF EXISTS title;
 ALTER TABLE movies DROP COLUMN IF EXISTS overview;
 
--- src/main/resources/db/migration/V16__make_original_title_and_original_name_not_null.sql
+++
@@ -1,5 +0,0 @@
-UPDATE movies SET original_title = title WHERE original_title IS NULL;
-UPDATE tv SET original_name = name WHERE original_name IS NULL;
-
-ALTER TABLE movies ALTER COLUMN original_title SET NOT NULL;
-ALTER TABLE tv ALTER COLUMN original_name SET NOT NULL;
