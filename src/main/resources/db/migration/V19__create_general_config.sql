c270cfbc2d34feebe6c2ff99c9c30f8dcdcb83f7
Carlos Gomez <carlosgomezcolmenero@gmail.com>
2026-09-12 00:58:50 +0200

    [REMEMBER_ME] Persist new remember me key gc

--- src/main/resources/db/migration/V19__create_general_config.sql
+++ src/main/resources/db/migration/V19__create_general_config.sql
@@ -1,4 +1,4 @@
-CREATE TABLE general_config (
+CREATE TABLE IF NOT EXISTS general_config (
     key   VARCHAR(50)  PRIMARY KEY,
     value VARCHAR(500) NOT NULL,
     updated_at_epoch_ms BIGINT NOT NULL
